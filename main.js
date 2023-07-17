2bf26e2aa8f19aafc894b6c5c487829f2b9d8096
Saharsh0 <113522933+Saharsh0@users.noreply.github.com>
2023-07-17 17:57:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,75 @@ document.getElementById("sketch_name").innerHTML = "sketch to be drawn: "+sketch
 
 timer_counter = 0
 timer_check =""
-drawn_skatch = ""
+drawn_sketch = ""
 answer_holder = ""
 score = 0
+
+function updateCanvas()
+{
+    background("white");
+    randomNumber1 = Math.floor((Math.random()*quick_draw_data_set.length)+1);
+    sketch = quick_draw_data_set[randomNumber1];
+    document.getElementById("sketch_name").innerHTML = "sketch to be drawn: "+sketch;
+}
+
+function preload()
+{
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function setup()
+{
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background('white');
+    canvas.mouseReleased(classifyCanvas);
+}
+
+function draw() {
+    strokeWeight(4);
+    stroke(0);
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+    check_sketch();
+
+    if (drawn_sketch == sketch) {
+        answer_holder = "set";
+        score++;
+        document.getElementById('score').innerHTML = "score: "+score;
+    }
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML = 'Label: '+results[0].label;
+    document.getElementById('confidence').innerHTML = 'Confidence: '+Math.round(results[0].confidence * 100) + '%'
+}
+
+function check_sketch()
+{
+    timer_counter++;
+    document.getElementById('time').innerHTML='time: '+timer_counter;
+    
+    if (timer_counter > 400)
+    {
+        timer_counter = 0;
+        timer_check = "completed";
+    }
+
+    if (timer_counter == "completed" || answer_holder == "set") {
+
+        timer_check = ""
+        answer_holder = ""
+        updateCanvas()
+    }
+}
+
